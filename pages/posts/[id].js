cbae1b1a89ae898d1aed1ec55eba8db1b683837c
Annie Huang <annie.hn@gmail.com>
2021-06-06 17:58:08 +1000

    Add getStaticPaths into [id].js

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,28 @@
 import Layout from '../../components/layout';
+import { getAllPostIds } from '../../lib/posts';
 
 export default function Post() {
   return <Layout>...</Layout>;
+}
+
+export async function getStaticPaths() {
+  // Returns an array that looks like this:
+  // [
+  //   {
+  //     params: {
+  //       id: 'ssg-ssr'
+  //     }
+  //   },
+  //   {
+  //     params: {
+  //       id: 'pre-rendering'
+  //     }
+  //   }
+  // ]
+  const paths = getAllPostIds();
+
+  return {
+    paths,
+    fallback: false,
+  };
 }
