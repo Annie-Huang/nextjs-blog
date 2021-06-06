b8fc42a158b304b746a57537fbf2077b824547c0
Annie Huang <annie.hn@gmail.com>
2021-06-06 16:57:22 +1000

    Update to use "Shu Uesugi" info

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,11 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>
+          Hello, I’m Shu. I’m a software engineer and a translator
+          (English/Japanese). You can contact me on{' '}
+          <a href='https://twitter.com/chibicode'>Twitter</a>.
+        </p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href='https://nextjs.org/learn'>our Next.js tutorial</a>.)
